f3946a14fc60b5145e2f769cee73b6af377be7b2
DummyDevPro <htun@osfl.ac.jp>
2022-06-15 17:05:58 +0900

    html home page file changes, login design changes

---
+++ js/app.js
@@ -0,0 +1,23 @@
+{/* <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBWWObXwVy9hoiqFd1iA4lc5esL3rGVJFw",
+    authDomain: "frontend-basics.firebaseapp.com",
+    projectId: "frontend-basics",
+    storageBucket: "frontend-basics.appspot.com",
+    messagingSenderId: "552240346186",
+    appId: "1:552240346186:web:b68b9af0df3d3dd60362b3",
+    measurementId: "G-00GZ8EM4HF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script> */}
